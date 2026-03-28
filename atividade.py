23d03e1f904179ae02446708b673786f15198abe
Mariaasxz <mvieiradasilva14@gmail.com>
2026-03-28 17:55:24 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,30 @@
+n1=int(input('Digite um numero: '))
+n2=int(input('Digite um numero: '))
+
+soma = n1 + n2 
+subtracao =  n1 - n2 
+multiplicacao= n1*n2
+
+divisao= n1/n2
+
+
+print (soma)
+print (subtracao)
+print (multiplicacao)
+print (divisao)
+
+
+
+
+produto =float(input('digite o valor '))
+# calcule um desconto de 10%
+valor=int(input('produto - 10%'))
+
+# mostre o valor final
+
+
+
+
+
+
+
